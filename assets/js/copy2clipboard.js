34a9bea8830c51ace756986f19de1ca0cd93e1ae
Pietro Ferraiuolo <167422164+pietroferraiuolo@users.noreply.github.com>
2024-06-18 14:39:51 +0200

    Update copy2clipboard.js

--- assets/js/copy2clipboard.js
+++ assets/js/copy2clipboard.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const emailLink = document.getElementById('emailLink');
+    const copyMessage = document.getElementById('copyMessage');
 
     emailLink.addEventListener('click', function (event) {
         event.preventDefault(); // Previene il comportamento di default del link
@@ -10,8 +11,16 @@ document.addEventListener('DOMContentLoaded', function () {
     function copyTextToClipboard(text) {
         navigator.clipboard.writeText(text).then(function () {
             console.log('Email copiata negli appunti: ', text);
+            showCopyMessage();
         }).catch(function (err) {
             console.error('Errore durante la copia dell\'email: ', err);
         });
+    }
+
+    function showCopyMessage() {
+        copyMessage.style.display = 'inline';
+        setTimeout(function () {
+            copyMessage.style.display = 'none';
+        }, 2000); // Nasconde il messaggio dopo 2 secondi
     }
 });
